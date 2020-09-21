698593c9ed825e79c67fccec18950bd045b88868
alexriv44 <71635787+alexriv44@users.noreply.github.com>
2020-09-21 17:44:43 +0200

    Update export.py
    executable='/bin/bash' at the end of the subprocess.call functions solved the issue #9

--- tools/_pypack/reconos/scripts/hw/export.py
+++ tools/_pypack/reconos/scripts/hw/export.py
@@ -186,7 +186,7 @@ def _export_hw_thread_ise_vivado(prj, hwdir, link, thread):
 				  cd {0};
 				  vivado_hls -f script_csynth.tcl;
 				  vivado -mode batch -notrace -nojournal -nolog -source script_vivado_edn.tcl;""".format(tmp.name, prj.impinfo.hls[1]),
-				  shell=True)
+				  shell=True, executable='/bin/bash')
 
 				dictionary = {}
 				dictionary["NAME"] = thread.name.lower()
@@ -205,10 +205,11 @@ def _export_hw_thread_ise_vivado(prj, hwdir, link, thread):
 
 		else:
 			subprocess.call("""
-			  source /opt/Xilinx/Vivado/{1}/settings64.sh;
+			  source /opt/
+			  /Vivado/{1}/settings64.sh;
 			  cd {0};
 			  vivado_hls -f script_csynth.tcl;""".format(tmp.name, prj.impinfo.hls[1]),
-			  shell=True)
+			  shell=True, executable='/bin/bash')
 
 			dictionary = {}
 			dictionary["NAME"] = thread.name.lower()
@@ -261,7 +262,7 @@ def _export_hw_vivado(prj, hwdir, link):
 					source /opt/Xilinx/Vivado/{1}/settings64.sh;
 					cd {0};
 					vivado -mode batch -notrace -nojournal -nolog -source create_ip_library.tcl;""".format(hwdir, prj.impinfo.xil[1]),
-					shell=True)
+					shell=True, executable='/bin/bash')
 	if result != 0 :
 		print("[RDK] Generation of Vivado IP repository failed. Maybe you specified unknown components in build.cfg?")
 		exit(1)
@@ -271,5 +272,5 @@ def _export_hw_vivado(prj, hwdir, link):
 					source /opt/Xilinx/Vivado/{1}/settings64.sh;
 					cd {0};
 					vivado -mode batch -notrace -nojournal -nolog -source export.tcl -tclargs -proj_name myReconOS -proj_path . ;""".format(hwdir, prj.impinfo.xil[1]),
-					shell=True)
+					shell=True, executable='/bin/bash')
 
